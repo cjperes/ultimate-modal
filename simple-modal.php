7fb873605b678a23faf19a0dcd64a7346b3d146e
claudiosmweb <contato@claudiosmweb.com>
2013-04-20 01:28:28 -0300

    created the plugin base

---
+++ simple-modal.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Simple Modal
+ * Plugin URI: http://claudiosmweb.com/
+ * Description: Displays a modal content in your WordPress
+ * Author: claudiosanches
+ * Author URI: http://claudiosmweb.com/
+ * Version: 0.1
+ * License: GPLv2 or later
+ * Text Domain: simplemodal
+ * Domain Path: /languages/
+ */
